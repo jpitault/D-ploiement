725a8a47f90c514b83e2ba8437f072be3bb1c5b1
root <julianptlt@gmail.com>
2018-10-16 11:28:51 -0400

    Mise à jour pour tous les OS

--- installation-xml.py
+++ installation-xml.py
@@ -36,19 +36,43 @@ else:
 	
 
 # On crée un host pour le DHCP, 1 seul script
-ajouterhost.ajouterhost(mac, os, ip, nom, file)
+#ajouterhost.ajouterhost(mac, os, ip, nom, file)
 
 
 # On crée un fichier pour le boot pxe
-if os == 'debian2':
+if os == 'debian2' or os == 'debian':
+	os = 'debian2'
+	# On crée un host pour le DHCP, 1 seul script
+	ajouterhost.ajouterhost(mac, os, ip, nom)
+	# On crée le fichier pxe
 	fichierspxe.pxedebian(mac)
+	# On crée le preseed
 	configinstall.debian(mac, mdp_root, nom_user, mdp_user, taille_swap)
 elif os == 'centos':
+	# On crée un host pour le DHCP, 1 seul script
+	ajouterhost.ajouterhost(mac, os, ip, nom)
 	fichierspxe.pxecentos(mac)
 	configinstall.centos(mac, mdp_root, nom_user, mdp_user, taille_swap)
 elif os == 'freebsd':
+	# On crée un host pour le DHCP, 1 seul script
+	ajouterhost.ajouterhost(mac, os, ip, nom)
 	fichierspxe.pxefreebsd(mac)
 	configinstall.freebsd(mac, taille_swap, nom, mdp_root, nom_user, mdp_user)
+elif os == 'ubuntu2' or os == 'ubuntu':
+	os = 'ubuntu2'
+	# On crée un host pour le DHCP, 1 seul script
+	ajouterhost.ajouterhost(mac, os, ip, nom)
+	fichierspxe.pxeubuntu(mac)
+	configinstall.ubuntu(mac, mdp_root, nom_user, mdp_user, taille_swap)
+elif os == 'proxmox':
+	os = 'debian2'
+	# On crée un host pour le DHCP, 1 seul script
+	ajouterhost.ajouterhost(mac, os, ip, nom)
+	fichierspxe.pxedebian(mac)
+	configinstall.proxmox(mac, mdp_root, nom_user, mdp_user, taille_swap)
+elif os == 'openbsd':
+	ajouterhost.ajouterhost(mac, os, ip, nom)
+	configinstall.openbsd(mac, nom, mdp_root, nom_user, mdp_user, taille_swap)
 else:
 	print('OS non supporté. Quitte le script')
-	sys.exit
+	sys.exit()
