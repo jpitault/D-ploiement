806b75e8977669a344731a0f9993bd1de9e56a0a
root <julianptlt@gmail.com>
2018-10-18 09:26:48 -0400

    Ajouter liste IP

--- apiDeploiement.py
+++ apiDeploiement.py
@@ -3,6 +3,8 @@ import uuid
 import os
 import installationxml as ix
 import suppressionhote as suph
+import xml.etree.ElementTree as ET
+
 
 
 class Hosts(object):
@@ -22,14 +24,12 @@ class Hosts(object):
 	# Get affiche tous les fichiers xml présents dans le dossier
 	def on_get(self, req, resp):
 		resp.status = falcon.HTTP_200
-		#allfiles = ['red', 'blue']
 		mbody = ''
 		for file in os.listdir(self._storage_path):
 			name = os.path.join(self._storage_path, file)
 			with open(name, 'r') as fichier:
 				contents = fichier.read()
 				mbody = mbody + file + '\n' + contents 
-		#	allfiles.append(contents)
 		resp.body = mbody
 		resp.content_type = falcon.MEDIA_XML
 		
@@ -89,9 +89,25 @@ class Host(object):
 			resp.status = falcon.HTTP_404
 			resp.body = message			
 			
+class UsedIP(object):
+	"""Affiche les IP utilisés par les fichiers xml
+	"""
+	def __init__(self, storage_path):
+		self._storage_path = storage_path
 		
-		
+	def on_get(self, req, resp):
+		tempbody = ''
+		for file in os.listdir(self._storage_path):
+			name_path = os.path.join(self._storage_path, file)
+			tree = ET.parse(name_path)
+			root = tree.getroot()
+			tempbody = tempbody + root[3].text + '	:	' + root[2].text + '\n'
+		resp.body = tempbody
+		resp.content_type = falcon.MEDIA_TEXT
+
+			
 api = falcon.API()
 
 api.add_route('/hosts', Hosts(storage_path='/root/fichiersxml'))
 api.add_route('/hosts/{name}', Host(storage_path='/root/fichiersxml'))
+api.add_route('/hosts/ip', UsedIP(storage_path='/root/fichiersxml'))
